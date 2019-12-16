89bce3927c7756b9282e4a5a6f1ba8b602d30299
adhe <tnils600@gmail.com>
2019-12-16 23:05:55 +0700

    change folder

--- Genius/application/views/templates/sidebar.php
+++ Genius/application/views/templates/sidebar.php
@@ -6,7 +6,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                 <i class="fas fa-book-reader"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Jenius Education</div>
+                <div class="sidebar-brand-text mx-3">Genius Education</div>
             </a>
 
             <!-- Divider -->
